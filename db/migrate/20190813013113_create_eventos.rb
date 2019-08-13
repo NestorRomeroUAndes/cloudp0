865f00e9d63f2664289b8144a63db8f585d43586
Nestor <ns.romerob@uniandes.edu.co>
2019-08-13 16:13:06 -0500

    m1

--- db/migrate/20190813013113_create_eventos.rb
+++ db/migrate/20190813013113_create_eventos.rb
@@ -1,14 +1,6 @@
 class CreateEventos < ActiveRecord::Migration[5.2]
   def change
     create_table :eventos do |t|
-      t.string :nombre
-      t.string :categoria
-      t.string :lugar
-      t.string :direccion
-      t.date :fecha_inicio
-      t.date :fecha_fin
-      t.string :modalidad
-
       t.timestamps
     end
   end
--- db/migrate/20190813203625_user_event_relation.rb
+++ db/migrate/20190813203625_user_event_relation.rb
@@ -1,6 +1,14 @@
 class UserEventRelation < ActiveRecord::Migration[5.2]
   def change
     change_table :eventos do |t|
+      t.string :nombre
+      t.string :categoria
+      t.string :lugar
+      t.string :direccion
+      t.date :fecha_inicio
+      t.date :fecha_fin
+      t.string :modalidad
+
       t.belongs_to :user, null: false, foreign_key: true
     end
   end
