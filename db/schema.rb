8fd3b450adeb1c4acd0f90d625a34467f2323aa2
root <root@ISIS4426-0042.sis.virtual.uniandes.edu.co>
2019-08-13 11:29:55 -0500

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_013113) do
+ActiveRecord::Schema.define(version: 2019_08_13_160742) do
 
   create_table "eventos", force: :cascade do |t|
     t.string "nombre"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2019_08_13_013113) do
     t.string "modalidad"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
