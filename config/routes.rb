4fe221af45d79f1cad3d51cce0ca2df07778bf4d
Nestor <ns.romerob@uniandes.edu.co>
2019-08-28 15:30:58 -0500

    f6

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'eventos/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :eventos
-  resource :designers, only[:index, :new, :create]
+  resource :designers, only: [:index, :new, :create]
   root 'eventos#index'
 end
