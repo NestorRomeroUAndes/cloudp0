88d46e2a67a78f7892034802ce7733bc59a9cf21
Nestor <ns.romerob@uniandes.edu.co>
2019-08-28 16:21:16 -0500

    f17

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,4 +1,4 @@
 class Design < ApplicationRecord
-  mount_uploader :Imagen, ImageUploader # Tells rails to use this uploader for this model.
+  mount_uploader :Imagen, ImagenUploader # Tells rails to use this uploader for this model.
   validates :Nombres, presence: true # Make sure the owner's name is present.
 end
