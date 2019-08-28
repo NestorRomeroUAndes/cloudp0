ce11f8833a25f903e6a43c005143b73238d7921c
Nestor <ns.romerob@uniandes.edu.co>
2019-08-28 15:18:53 -0500

    f4

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -3,7 +3,21 @@ class DesignersController < ApplicationController
     @designs = designs.all
   end
 
+  def show
+    @design = designs.find(params[:id])
+  end
+
   def new
     @design = design.new
+  end
+
+  def create
+    @design = design.new(design_params)
+
+    if @design.save
+      redirect_to designers_path, notice: "Diseño cargado exitosamente"
+    else
+      render 'new'
+    end
   end
 end
