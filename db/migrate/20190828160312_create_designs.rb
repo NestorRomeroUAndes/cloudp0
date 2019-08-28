df3943b0a33374ed43af27affbe023f8c8ffd2ed
Nestor <ns.romerob@uniandes.edu.co>
2019-08-28 11:12:55 -0500

    f1

--- db/migrate/20190828160312_create_designs.rb
+++ db/migrate/20190828160312_create_designs.rb
@@ -1,6 +1,11 @@
 class CreateDesigns < ActiveRecord::Migration[5.2]
   def change
     create_table :designs do |t|
+      t.string :Nombres
+      t.string :Apellidos
+      t.string :email
+      #agregar el campo de archivo
+      t.bigint :Precio
 
       t.timestamps
     end
