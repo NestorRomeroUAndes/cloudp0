e4abd62ddfcf6c44b81439431ac99fbee973e4fe
Nestor <ns.romerob@uniandes.edu.co>
2019-09-01 02:32:33 -0500

    g4

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,7 +1,30 @@
 class DesignersController < ApplicationController
   def index
     @designers = Design.all
-    Resque.enqueue(Resizer)
+    #Resque.enqueue(Resizer)
+
+    @pendingDesigns = Design.all
+    @pendingDesigns.each do |pd|
+      pd.Estado = "Disponible"
+      img = pd.Imagen.cover.file.file
+      mrk = pd.Nombres + " " + pd.Apellidos + " " + pd.created_at.to_s
+      rimg = ::Magick::Image.read(img).first
+      cover = rimg.resize(800, 600)
+      mark = Magick::Image.new(cover.columns, cover.rows) {self.background_color = "none"}
+      draw = Magick::Draw.new
+      draw.annotate(mark, 0, 0, 0, 0, mrk) do
+        # place the text in the centre of the canvas
+        draw.gravity = Magick::CenterGravity
+        # set text height in points where 1 point is 1/72 inches
+        draw.pointsize = 50
+        draw.font_family = "Times" # set font
+        draw.fill = "black" # set text color
+        draw.stroke = "none" # remove stroke
+      end
+      cover = cover.watermark(mark, 0.2, 0.5, Magick::CenterGravity)
+      cover.write(img)
+      pd.save
+    end
   end
 
   def new
