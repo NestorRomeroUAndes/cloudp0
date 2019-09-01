8aff2853e110adb41ebcdca2661c1a26aa52c781
Nestor <ns.romerob@uniandes.edu.co>
2019-09-01 01:43:38 -0500

    r2

--- app/jobs/resizer.rb
+++ app/jobs/resizer.rb
@@ -8,7 +8,7 @@ class Resizer
       pd.Estado = "Disponible"
       img = pd.Imagen.cover.file.file
       mrk = pd.Nombres + " " + pd.Apellidos + " " + pd.created_at.to_s
-      rimg = Magick::Image.read(img).first
+      rimg = ::Magick::Image.read(img).first
       cover = rimg.resize(800, 600)
       mark = Magick::Image.new(cover.columns, cover.rows) {self.background_color = "none"}
       draw = Magick::Draw.new
@@ -16,7 +16,7 @@ class Resizer
         # place the text in the centre of the canvas
         draw.gravity = Magick::CenterGravity
         # set text height in points where 1 point is 1/72 inches
-        draw.pointsize = 100
+        draw.pointsize = 11
         draw.font_family = "Times" # set font
         draw.fill = "black" # set text color
         draw.stroke = "none" # remove stroke
