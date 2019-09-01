883439df6d31229413c98ec9947de303ade77565
Nestor <ns.romerob@uniandes.edu.co>
2019-09-01 01:46:47 -0500

    t5

--- app/jobs/resizer.rb
+++ app/jobs/resizer.rb
@@ -10,18 +10,18 @@ class Resizer
       mrk = pd.Nombres + " " + pd.Apellidos + " " + pd.created_at.to_s
       rimg = ::Magick::Image.read(img).first
       cover = rimg.resize(800, 600)
-      mark = Magick::Image.new(cover.columns, cover.rows) {self.background_color = "none"}
-      draw = Magick::Draw.new
-      draw.annotate(mark, 0, 0, 0, 0, mrk) do
-        # place the text in the centre of the canvas
-        draw.gravity = Magick::CenterGravity
-        # set text height in points where 1 point is 1/72 inches
-        draw.pointsize = 11
-        draw.font_family = "Times" # set font
-        draw.fill = "black" # set text color
-        draw.stroke = "none" # remove stroke
-      end
-      cover = cover.watermark(mark, 0.2, 0.5, Magick::CenterGravity)
+      # mark = Magick::Image.new(cover.columns, cover.rows) {self.background_color = "none"}
+      # draw = Magick::Draw.new
+      # draw.annotate(mark, 0, 0, 0, 0, mrk) do
+      #   # place the text in the centre of the canvas
+      #   draw.gravity = Magick::CenterGravity
+      #   # set text height in points where 1 point is 1/72 inches
+      #   draw.pointsize = 11
+      #   draw.font_family = "Times" # set font
+      #   draw.fill = "black" # set text color
+      #   draw.stroke = "none" # remove stroke
+      # end
+      # cover = cover.watermark(mark, 0.2, 0.5, Magick::CenterGravity)
       cover.write(img)
       pd.save
     end
