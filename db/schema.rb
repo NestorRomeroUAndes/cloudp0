40ac36f73f25e8326fb52c6b6f21ab3d027cd83f
root <root@ISIS4426-0042.sis.virtual.uniandes.edu.co>
2019-09-03 13:03:09 -0500

    m

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_160312) do
-
-  create_table "designs", force: :cascade do |t|
-    t.string "Nombres"
-    t.string "Apellidos"
-    t.string "Email"
-    t.string "Imagen"
-    t.bigint "Precio"
-    t.string "Estado"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "eventos", force: :cascade do |t|
-    t.string "nombre"
-    t.string "categoria"
-    t.string "lugar"
-    t.string "direccion"
-    t.date "fecha_inicio"
-    t.date "fecha_fin"
-    t.string "modalidad"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_eventos_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "empresa", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", limit: 128, null: false
-    t.string "confirmation_token", limit: 128
-    t.string "remember_token", limit: 128, null: false
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["remember_token"], name: "index_users_on_remember_token"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
