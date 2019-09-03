49dbe3e916f254395bf7c9566305f8825e5c5e97
Nestor <ns.romerob@uniandes.edu.co>
2019-09-03 09:35:31 -0500

    f1
    f1

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :eventos
   resource :designers, only: [:index, :new, :create]
-  root 'eventos#index'
+  root 'welcome#index'
   mount Resque::Server.new, at: "/resque"
 end
