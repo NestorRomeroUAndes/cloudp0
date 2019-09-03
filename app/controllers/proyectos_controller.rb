31d9cc688c7b0fc2b510b22ee18c90869c46a442
Nestor <ns.romerob@uniandes.edu.co>
2019-09-03 11:42:02 -0500

    dd

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -2,23 +2,23 @@ class ProyectosController < ApplicationController
   before_action :require_login
 
   def index
-    @proyectos = current_user.proyecto.order(id: :desc)
+    @proyectos = current_user.proyectos.order(id: :desc)
   end
 
   def show
-    @proyecto = current_user.proyecto.find(params[:id])
+    @proyecto = current_user.proyectos.find(params[:id])
   end
 
   def edit
-    @proyecto = current_user.proyecto.find(params[:id])
+    @proyecto = current_user.proyectos.find(params[:id])
   end
 
   def new
-     @proyecto = current_user.proyecto.build
+     @proyecto = current_user.proyectos.build
   end
 
   def create
-    @proyecto = current_user.proyecto.build(evento_params)
+    @proyecto = current_user.proyectos.build(evento_params)
     if @proyecto.save
       redirect_to @proyecto
     else
@@ -27,7 +27,7 @@ class ProyectosController < ApplicationController
   end
 
   def update
-    @proyecto = current_user.proyecto.find(params[:id])
+    @proyecto = current_user.proyectos.find(params[:id])
 
     if @proyecto.update(evento_params)
       redirect_to @proyecto
@@ -37,14 +37,14 @@ class ProyectosController < ApplicationController
   end
 
   def destroy
-    @proyecto = current_user.proyecto.find(params[:id])
+    @proyecto = current_user.proyectos.find(params[:id])
     @proyecto.destroy
 
     redirect_to proyectos_path
   end
 
   private
-  def proyecto_params
+  def proyectos_params
     params.require(:proyecto).permit(:nombre, :descripcion, :valor)
   end
 end
