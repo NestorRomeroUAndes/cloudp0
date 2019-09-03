c93ab8238252441df865aee5ca8ba8859ad0ae10
Nestor <ns.romerob@uniandes.edu.co>
2019-09-03 11:37:19 -0500

    m1

--- db/migrate/20190903161303_create_proyectos.rb
+++ db/migrate/20190903161303_create_proyectos.rb
@@ -1,8 +1,8 @@
 class CreateProyectos < ActiveRecord::Migration[5.2]
   def change
     create_table :proyectos do |t|
-      t.string, :nombre
-      t.string, :descripcion
+      t.string :nombre
+      t.string :descripcion
       t.bigint :valor
 
       t.belongs_to :user, null: false, foreign_key: true
