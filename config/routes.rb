b659e9843b06fec8c09ccd21fb265c27820f76d1
Nestor <ns.romerob@uniandes.edu.co>
2019-09-03 09:40:52 -0500

    f2
    f2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'resque/server'
 Rails.application.routes.draw do
   get 'designers/index'
   get 'eventos/index'
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :eventos
   resource :designers, only: [:index, :new, :create]
