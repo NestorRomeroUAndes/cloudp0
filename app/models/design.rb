a393625d2912082d68109c1f69cfe390f2d3edd4
Nestor <ns.romerob@uniandes.edu.co>
2019-09-03 22:07:59 -0500

    gsfd

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,9 +1,10 @@
 class Design < ApplicationRecord
   mount_uploader :Imagen, ImageUploader # Tells rails to use this uploader for this model.
   validates :Nombres, presence: true # Make sure the owner's name is present.
+  validates :Imagen, presence: true
   belongs_to :proyecto
   before_save :default_values
   def default_values
-    self.Estado ||= 'pendiente' 
+    self.Estado ||= 'pendiente'
   end
 end
