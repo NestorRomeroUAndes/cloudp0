be3e68500645235dfd5793733e070f9096cea974
Nestor <ns.romerob@uniandes.edu.co>
2019-09-05 16:06:34 -0500

    fdgh

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
   def vanity
     @empresa = "-"
     if !params[:usrid].blank?
-      @proyectos = User.find(params[:usrid]).proyecto.include(:design).order(id: :desc)
+      @proyectos = User.find(params[:usrid]).proyecto.order(id: :desc)
       # @proyectos = User.find(params[:usrid]).proyecto.joins(:design).order('id DESC').paginate(:page => params[:page], :per_page => 10)
       #@proyectos = @designs.group_by(&:nombre)
       @empresa = User.find(params[:usrid]).empresa
